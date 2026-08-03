30b619c41ab31ff190b4e0c2aada6b9b6cf1ce18
Zenn <suzen5029@gmail.com>
2026-08-03 23:10:51 +0200

    Add files via upload

---
+++ src/client/java/hu/originsmp/mod/client/OriginSMPClient.java
@@ -0,0 +1,10 @@
+package hu.originsmp.mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OriginSMPClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
