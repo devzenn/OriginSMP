63b3f7fe339f1fa5681f0be822b660d939f727ac
Zenn <suzen5029@gmail.com>
2026-08-04 13:41:37 +0200

    Update OriginSMPClient.java

--- src/client/java/hu/originsmp/mod/client/OriginSMPClient.java
+++ src/client/java/hu/originsmp/mod/client/OriginSMPClient.java
@@ -1,10 +1,16 @@
 package hu.originsmp.mod.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import hu.originsmp.mod.AbilityKeybinds;
+import hu.originsmp.mod.AbilityHandler;
 
 public class OriginSMPClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+
+    @Override
+    public void onInitializeClient() {
+
+        AbilityKeybinds.register();
+        AbilityHandler.register();
+
+    }
 }
